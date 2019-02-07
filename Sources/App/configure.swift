6b3082aeec893f5709ab5e38232198938e2f11f0
Peter Jeffs <peterjeffs@Peters-MacBook-Pro.local>
2019-02-07 16:22:41 +0000

    Use PostgreSQL as the DB

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -17,13 +17,19 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
     middlewares.use(ErrorMiddleware.self) // Catches errors and converts to HTTP response
     services.register(middlewares)
 
-    // Configure a database
+    // Configure a PostgreSQL database
     var databases = DatabasesConfig()
+    let hostname = Environment.get("DATABASE_HOSTNAME") ?? "localhost"
+    let username = Environment.get("DATABASE_USER") ?? "vapor"
+    let databaseName = Environment.get("DATABASE_DB") ?? "vapor"
+    let password = Environment.get("DATABASE_PASSWORD") ?? "password"
+    //
     let databaseConfig = PostgreSQLDatabaseConfig(
-        hostname: "localhost",
-        username: "vapor",
-        database: "vapor",
-        password: "password")
+        hostname: hostname,
+        username: username,
+        database: databaseName,
+        password: password)
+    //
     let database = PostgreSQLDatabase(config: databaseConfig)
     databases.add(database: database, as: .psql)
     services.register(databases)
